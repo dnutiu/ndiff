283efb0b1f49053944b7a46818754d43fa5f2a42
Denis Nutiu <denis.nutiu@gmail.com>
2024-10-24 21:27:27 +0300

    refactor using Enum

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
-use crate::line::{DifferingLine, Line, MatchedLine};
+use crate::line::{Line, MissingLineIndicator};
 use itertools::Itertools;
 use std::fs::read_to_string;
-use std::ops::Deref;
 
 mod line;
 
 /// Gets the lines of two given texts
 ///
-/// It returns a [`Vec<Box<dyn Line>>`]
-fn get_lines(left_text: &String, right_text: &String) -> Vec<Box<dyn Line>> {
+/// It returns a [`Vec<Box<Line>>`]
+fn get_lines(left_text: &String, right_text: &String) -> Vec<Line> {
     left_text
         .lines()
         .map(String::from)
         .zip_longest(right_text.lines().map(String::from))
         .enumerate()
-        .map(|(line, item)| -> Box<dyn Line> {
-            let left = item.clone().left().unwrap_or(String::from(""));
-            let right = item.right().unwrap_or(String::from(""));
+        .map(|(line_number, item)| -> Line {
+            let missing_line_indicator: MissingLineIndicator = Default::default();
+            let left = item.clone().left().unwrap_or(missing_line_indicator.value.clone());
+            let right = item.right().unwrap_or(missing_line_indicator.value);
 
             if left != right {
-                Box::new(DifferingLine::new(line as i32 + 1, left, right))
+                Line::DifferingLine(line_number as i32 + 1, left, right)
             } else {
-                Box::new(MatchedLine::new(line as i32 + 1, left))
+                Line::MatchedLine(line_number as i32 + 1, left)
             }
         })
         .collect()
@@ -31,14 +31,16 @@ fn main() {
     let left_file = read_to_string("a.txt").expect("Left file not found");
     let right_file = read_to_string("b.txt").expect("Right file not found");
 
-    let lines: Vec<Box<dyn Line>> = get_lines(&left_file, &right_file);
+    let lines: Vec<Line> = get_lines(&left_file, &right_file);
 
     let mut differences_counter: i32 = 0;
     for line in lines.iter() {
-        let line = line.deref();
         print!("{}", line);
-        if line.is_differing() {
-            differences_counter += 1;
+        match line {
+            Line::MatchedLine(_, _) => {}
+            Line::DifferingLine(_, _, _) => {
+                differences_counter += 1
+            }
         }
     }
     println!();
